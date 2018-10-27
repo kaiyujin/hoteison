ca50b23fcfbb6e1e176f8e301e6511586f5e236b
daisuke shiraishi <kaiyujin@gmail.com>
2018-10-28 00:25:26 +0900

    add nuxt2

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center>
+    <v-flex
+      xs12
+      sm8
+      md6>
+      <div class="text-xs-center">
+        <logo/>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
