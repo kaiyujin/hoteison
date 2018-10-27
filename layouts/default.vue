993c74874b8ab3a537747a52aa06b389d2157256
daisuke shiraishi <kaiyujin@gmail.com>
2018-10-28 01:05:02 +0900

    change style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,86 +1,111 @@
 <template>
-  <v-app dark>
+  <v-app id="keep">
     <v-navigation-drawer
-      :mini-variant="miniVariant"
-      :clipped="clipped"
       v-model="drawer"
       fixed
+      clipped
+      class="grey lighten-4"
       app
     >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :to="item.to"
-          :key="i"
-          router
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"/>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"/>
-          </v-list-tile-content>
-        </v-list-tile>
+      <v-list
+        dense
+        class="grey lighten-4"
+      >
+        <template v-for="(item, i) in items">
+          <v-layout
+            v-if="item.heading"
+            :key="i"
+            row
+            align-center
+          >
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-flex>
+            <v-flex
+              xs6
+              class="text-xs-right">
+              <v-btn
+                small
+                flat>edit</v-btn>
+            </v-flex>
+          </v-layout>
+          <v-divider
+            v-else-if="item.divider"
+            :key="i"
+            dark
+            class="my-3"
+          />
+          <v-list-tile
+            v-else
+            :key="i"
+            @click="alert('test')"
+          >
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title class="grey--text">
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar
-      :clipped-left="clipped"
-      fixed
+      color="amber"
       app
-    >
-      <v-toolbar-side-icon @click="drawer = !drawer"/>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"/>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"/>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
+      absolute
+      clipped-left>
+      <v-toolbar-side-icon @click.native="drawer = !drawer"/>
+      <span class="title ml-3 mr-5">Hoteison</span>
+      <v-spacer/>
     </v-toolbar>
     <v-content>
-      <v-container>
-        <nuxt/>
+      <v-container
+        fluid
+        fill-height
+        class="grey lighten-4">
+        <v-layout
+          justify-center
+          align-center>
+          <v-flex shrink>
+            test
+          </v-flex>
+        </v-layout>
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      :right="right"
-      v-model="rightDrawer"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2018</span>
-    </v-footer>
   </v-app>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    items: [
+      { icon: 'lightbulb_outline', text: 'Notes' },
+      { icon: 'touch_app', text: 'Reminders' },
+      { divider: true },
+      { heading: 'Labels' },
+      { icon: 'add', text: 'Create new label' },
+      { divider: true },
+      { icon: 'archive', text: 'Archive' },
+      { icon: 'delete', text: 'Trash' },
+      { divider: true },
+      { icon: 'settings', text: 'Settings' },
+      { icon: 'chat_bubble', text: 'Trash' },
+      { icon: 'help', text: 'Help' },
+      { icon: 'phonelink', text: 'App downloads' },
+      { icon: 'keyboard', text: 'Keyboard shortcuts' }
+    ]
+  })
+}
+</script>
+
+<style lang="stylus">
+  #keep
+    .v-navigation-drawer__border
+      display: none
+</style>
