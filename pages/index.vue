018e7c33a93cf334b56b4397047e8df641392b0e
daisuke shiraishi <kaiyujin@gmail.com>
2018-10-29 05:16:14 +0900

    add login

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,64 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center>
-    <v-flex
-      xs12 
-      sm8
-      md6>
-      <div class="text-xs-center">
-        <logo/>
-      </div>
-    </v-flex>
-  </v-layout>
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        fluid
+        fill-height>
+        <v-layout
+          align-center
+          justify-center>
+          <v-flex
+            xs12
+            sm8
+            md4>
+            <v-card class="elevation-12">
+              <v-toolbar
+                dark
+                color="primary">
+                <v-toolbar-title>Hoteison Login form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    v-model="email"
+                    prepend-icon="person"
+                    name="email"
+                    label="Email"
+                    type="text"/>
+                  <v-text-field
+                    v-model="password"
+                    prepend-icon="lock"
+                    name="password"
+                    label="Password"
+                    type="password"/>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer/>
+                <v-btn
+                  color="primary"
+                  @click="login">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  layout: 'login',
+  data: () => ({
+    email: '',
+    password: ''
+  }),
+  methods: {
+    login() {
+      alert('hoge')
+      return null
+    }
   }
 }
 </script>
