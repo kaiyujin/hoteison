a3c1db09dd3d661b2adcad38b581fe572595444b
daisuke shiraishi <kaiyujin@gmail.com>
2018-10-31 01:55:26 +0900

    get titles

--- layouts/default.vue
+++ layouts/default.vue
@@ -99,12 +99,16 @@ export default {
     userData: null
   }),
   asyncData() {
-    firebase.firestore().collection('titles').get().then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
+    firebase
+      .firestore()
+      .collection('titles')
+      .get()
+      .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, ' => ', doc.data())
+        })
       })
-    })
     return {}
   },
   created: function() {
--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,30 @@
       sm8
       md6>
       <div class="text-xs-center"/>
+      {{ titles }}
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-export default {}
+import firebase from '@/plugins/firebase'
+export default {
+  async asyncData() {
+    let titles = []
+    await firebase
+      .firestore()
+      .collection('titles')
+      .get()
+      .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, ' => ', doc.data())
+          titles.push(doc.data().name)
+        })
+      })
+    return {
+      titles
+    }
+  }
+}
 </script>
